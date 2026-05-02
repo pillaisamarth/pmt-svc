a916de9b933183985d9f25a519e85435cc0f6392
Samarth Pillai <pillaisamarth@gmail.com>
2026-05-03 01:53:57 +0530

    Add idempotency check based on order id

--- src/main/java/com/sp/pmt_svc/kafka/OrderListener.java
+++ src/main/java/com/sp/pmt_svc/kafka/OrderListener.java
@@ -3,12 +3,18 @@ package com.sp.pmt_svc.kafka;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 @Slf4j
 public class OrderListener {
+    private final Map<String, Boolean> processedOrderIds = new ConcurrentHashMap<>();
 
-    @KafkaListener(topics = "order-placed", errorHandler = "customErrorHandler")
+    @KafkaListener(topics = "order-placed", errorHandler = "customErrorHandler", concurrency = "3")
     public void listenOrderPlaced(String message){
         log.info("Received OrderPlaced event: {}", message);
 
@@ -17,6 +23,14 @@ public class OrderListener {
             throw new RuntimeException("Payment processing failed");
         }
 
+        JsonNode node = new ObjectMapper().readTree(message);
+        String orderId = String.valueOf(node.get("orderId"));
+
+        Boolean processed = processedOrderIds.putIfAbsent(orderId, true);
+        if(processed != null){
+            log.info("Skipping duplicate orderId: {}", orderId);
+            return;
+        }
         log.info("Successfully processed order");
     }
 }
