bb10d83d8c5aa487c99749b2a18f46195e9b82fe
Samarth Pillai <pillaisamarth@gmail.com>
2026-05-07 01:50:07 +0530

    Add dlt when exception thrown in container

--- src/main/java/com/sp/pmt_svc/handlers/KafkaErrorHandlerConfig.java
+++ src/main/java/com/sp/pmt_svc/handlers/KafkaErrorHandlerConfig.java
@@ -1,19 +1,42 @@
 package com.sp.pmt_svc.handlers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.kafka.autoconfigure.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.listener.ConsumerAwareListenerErrorHandler;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.BackOff;
+import org.springframework.util.backoff.FixedBackOff;
 
 @Configuration
 @Slf4j
 public class KafkaErrorHandlerConfig {
 
+    //this error handler is invoked when an error is thrown and not caught in listener. If not
+    //caught this will propagate to listener container (kafkalistenercontainer). Even when we're
+    //using concurrent kafka listener container, it'll just work as parent or master and the actual
+    // kafka listener method has kafka listener container wrapped around. concurrency of 6 means
+    // there will be 6 kafka listener container that has kafkaconsumer (performing polls) wrapped
+    // within it, and this container will call the listener method (annotated with @kafkalistener)
+    // after fetching the record
     @Bean
     public ConsumerAwareListenerErrorHandler customErrorHandler(){
         return (msg, ex, consumer) -> {
             log.error("Exception thrown in the listener while processing message", ex);
-            return msg;
+            throw ex;
         };
+    }
+
+    //this error handler is invoked when error is thrown to kafka listernr contianer
+    //default dead-letter topic is original topic + "-dlt"
+    @Bean
+    public DefaultErrorHandler defaultErrorHandler(KafkaTemplate<Object, Object> template){
+        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(template);
+        BackOff backOff = new FixedBackOff(3000L, 2L);
+
+        return new DefaultErrorHandler(recoverer, backOff);
     }
 }
---
+++ src/main/java/com/sp/pmt_svc/kafka/DeadLetterTopicListener.java
@@ -0,0 +1,20 @@
+package com.sp.pmt_svc.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class DeadLetterTopicListener {
+
+    //each listener(like this) gets 3(concurrency) kafkalistenercontainer (threads)
+    //these threads are used by consumers to consume messages and are not shared by other
+    // listeners. For example the 3 threads (and as a result 3 kafkalistenercontainer) will
+    // just be used to consume the topics mentioned in this listener.
+    // these 3 kafkalistenercontainer will be managed by concurrentkafkalistenrcontainer.
+    @KafkaListener(topics = "order-placed-dlt", concurrency = "3")
+    public void receiveOrderDlt(String message){
+        log.info("Received message in dead-letter-topic: {}", message);
+    }
+}
